83d410e146b59c5ca4261cdc1938000723d81a72
MariaEduardaFeledi <94013529+MariaEduardaFeledi@users.noreply.github.com>
2022-04-20 16:46:55 -0300

    Refactoring 5: Replace Temp With Query

--- src/index.js
+++ src/index.js
@@ -144,11 +144,38 @@ class Customer {
 
             //show figures for this rental
             result += `\t${rental.movie.title}\t${rental.getCharge()}\n`;
-            totalAmount += rental.getCharge();
         }
 
         //add footer lines
-        result += `Amount owed is ${totalAmount}\nYou earned ${frequentRenterPoints} frequent renter points`;
+        result += `Amount owed is ${this.getTotalCharge()}\nYou earned ${this.getTotalFrequentRenterPoints()} frequent renter points`;
+        return result;
+    }
+
+    /**
+     * @method getTotalCharge
+     * @return {number}
+     */
+     getTotalCharge() {
+        let result = 0;
+        
+        for (let rental of this.rentals) {
+            result += rental.getCharge();
+        }
+
+        return result;
+    }
+
+    /**
+     * @method getTotalFrequentRenterPoints
+     * @return {number}
+     */
+    getTotalFrequentRenterPoints() {
+        let result = 0;
+        
+        for (let rental of this.rentals) {
+            result += rental.getFrequentRenterPoints();
+        }
+
         return result;
     }
 }
