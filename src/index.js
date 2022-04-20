24e1a4f6548d1dac3b3accd3e1c2f0bfe9adc522
MariaEduardaFeledi <94013529+MariaEduardaFeledi@users.noreply.github.com>
2022-04-20 16:26:45 -0300

    Refactoring #1: Extract Method

--- src/index.js
+++ src/index.js
@@ -91,6 +91,34 @@ class Customer {
         this._rentals.push(rental);
     }
 
+      /**
+     * @param {Rental} rental
+     * @return {number}
+     */
+    amountFor(rental) {
+        let amount = 0;
+
+        switch (rental.movie.priceCode) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (rental.daysRented > 2) {
+                    amount += (rental.daysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                amount += rental.daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                    amount += 1.5;
+                if (rental.daysRented > 3) {
+                    amount += (rental.daysRented - 3) * 1.5;
+                }
+                break;
+        }
+
+        return amount;
+    }
+
     /**
      * @method statement
      * @return {string}
@@ -102,26 +130,7 @@ class Customer {
         let result = `Rental Record for ${this.name}\n`;
 
         for (let rental of this.rentals) {
-            let thisAmount = 0;
-
-            // Determine amounts for each line
-            switch (rental.movie.priceCode) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (rental.daysRented > 2) {
-                        thisAmount += (rental.daysRented - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += rental.daysRented * 3;
-                    break;
-                case Movie.CHILDREN:
-                    thisAmount += 1.5;
-                    if (rental.daysRented > 3) {
-                        thisAmount += (rental.daysRented - 3) * 1.5;
-                    }
-                    break;
-            }
+            let thisAmount = this.amountFor(rental);
 
             frequentRenterPoints++;
 
